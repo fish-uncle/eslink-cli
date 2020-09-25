aa55bed5a3fe87a86b5b753f8cf84f456b3357af
zhepei.shen <zhepei.shen@eslink.com>
2020-09-25 16:26:11 +0800

    '优化打包文件大小'

--- bin/react/config/webpack.base.config.js
+++ bin/react/config/webpack.base.config.js
@@ -1,58 +1,60 @@
 'use strict'
-const path = require ('path')
-const config = require ('../../../config/react.config')
-const node_modules = path.resolve (__dirname, '../../../node_modules')
-const resolve = require ('../../../config/resolve')
-const devServer = require ('../../../config/devServer')
-const styleRules = require ('../../../config/rules/styleRules')
+const path = require('path')
+const config = require('../../../config/react.config')
+const node_modules = path.resolve(__dirname, '../../../node_modules')
+const resolve = require('../../../config/resolve')
+const devServer = require('../../../config/devServer')
+const styleRules = require('../../../config/rules/styleRules')
 
 module.exports = {
-  entry: [ config.entry ],
-  optimization: {
-    runtimeChunk: {
-      name: 'manifest'
-    },
-    splitChunks: {
-      cacheGroups: {
-        common: {
-          chunks: 'initial',
-          name: 'common',
-          minChunks: 2,
-          maxInitialRequests: 5,
-          minSize: 0
-        },
-        vendor: {
-          test: /node_modules/,
-          chunks: 'initial',
-          name: 'vendor',
-          priority: 10,
-          enforce: true
-        }
-      }
-    }
-  },
-  resolve: resolve ('react'),
-  devServer: devServer ('react'),
-  module: {
-    rules: [
-      {
-        test: /\.md$/,
-        loader: [ `${node_modules}/html-loader`, `${node_modules}/markdown-loader` ],
-      },
-      {
-        test: /\.tsx|ts?$/, loader: `${node_modules}/awesome-typescript-loader`
-      },
-      {
-        test: /\.js|jsx$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: `${node_modules}/babel-loader`,
-        options: { presets: config.babelPresets, plugins: config.babelPlugins }
-      },
-      ...styleRules ('react'),
-      ...config.rules
-    ]
-  },
-  plugins: [
-    ...config.webpackPlugin
-  ]
+	entry: [config.entry],
+	optimization: {
+		runtimeChunk: {
+			name: 'manifest'
+		},
+		splitChunks: {
+			cacheGroups: {
+				common: {
+					chunks: 'initial',
+					name: 'common',
+					minChunks: 1,
+					maxAsyncRequests: 30,
+					maxInitialRequests: 30,
+					maxSize: 200 * 1024,
+				},
+				vendor: {
+					test: /node_modules/,
+					chunks: 'initial',
+					name: 'vendor',
+					priority: 10,
+					maxSize: 200 * 1024,
+					enforce: true
+				}
+			}
+		}
+	},
+	resolve: resolve('react'),
+	devServer: devServer('react'),
+	module: {
+		rules: [
+			{
+				test: /\.md$/,
+				loader: [`${node_modules}/html-loader`, `${node_modules}/markdown-loader`],
+			},
+			{
+				test: /\.tsx|ts?$/, loader: `${node_modules}/awesome-typescript-loader`
+			},
+			{
+				test: /\.js|jsx$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: `${node_modules}/babel-loader`,
+				options: {presets: config.babelPresets, plugins: config.babelPlugins}
+			},
+			...styleRules('react'),
+			...config.rules
+		]
+	},
+	plugins: [
+		...config.webpackPlugin
+	]
 }
--- bin/vue/config/webpack.base.config.js
+++ bin/vue/config/webpack.base.config.js
@@ -1,63 +1,65 @@
 'use strict'
-const path = require ('path')
-const VueLoaderPlugin = require ('vue-loader/lib/plugin')
-const config = require ('../../../config/vue.config')
-const resolve = require ('../../../config/resolve')
-const devServer = require ('../../../config/devServer')
-const styleRules = require ('../../../config/rules/styleRules')
-const eslintRules = require ('../../../config/rules/eslintRules')
-const node_modules = path.resolve (__dirname, '../../../node_modules')
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const config = require('../../../config/vue.config')
+const resolve = require('../../../config/resolve')
+const devServer = require('../../../config/devServer')
+const styleRules = require('../../../config/rules/styleRules')
+const eslintRules = require('../../../config/rules/eslintRules')
+const node_modules = path.resolve(__dirname, '../../../node_modules')
 
 module.exports = {
-  entry: [ config.entry ],
-  optimization: {
-    runtimeChunk: {
-      name: 'manifest'
-    },
-    splitChunks: {
-      cacheGroups: {
-        common: {
-          chunks: 'initial',
-          name: 'common',
-          minChunks: 2,
-          maxInitialRequests: 5,
-          minSize: 0
-        },
-        vendor: {
-          test: /node_modules/,
-          chunks: 'initial',
-          name: 'vendor',
-          priority: 10,
-          enforce: true
-        }
-      }
-    }
-  },
-  resolve: resolve ('vue'),
-  devServer: devServer ('vue'),
-  module: {
-    rules: [
-      ...eslintRules,
-      {
-        test: /\.md$/,
-        loader: [ `${node_modules}/html-loader`, `${node_modules}/markdown-loader` ],
-      },
-      {
-        test: /\.vue$/,
-        loader: [ `${node_modules}/vue-loader` ],
-      },
-      {
-        test: /\.js|jsx$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: `${node_modules}/babel-loader`,
-        options: { presets: config.babelPresets, plugins: config.babelPlugins }
-      },
-      ...styleRules ('vue'),
-      ...config.rules
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin (),
-    ...config.webpackPlugin
-  ]
+	entry: [config.entry],
+	optimization: {
+		runtimeChunk: {
+			name: 'manifest'
+		},
+		splitChunks: {
+			cacheGroups: {
+				common: {
+					chunks: 'initial',
+					name: 'common',
+					minChunks: 1,
+					maxAsyncRequests: 30,
+					maxInitialRequests: 30,
+					maxSize: 200 * 1024,
+				},
+				vendor: {
+					test: /node_modules/,
+					chunks: 'initial',
+					name: 'vendor',
+					priority: 10,
+					maxSize: 200 * 1024,
+					enforce: true
+				}
+			}
+		}
+	},
+	resolve: resolve('vue'),
+	devServer: devServer('vue'),
+	module: {
+		rules: [
+			...eslintRules,
+			{
+				test: /\.md$/,
+				loader: [`${node_modules}/html-loader`, `${node_modules}/markdown-loader`],
+			},
+			{
+				test: /\.vue$/,
+				loader: [`${node_modules}/vue-loader`],
+			},
+			{
+				test: /\.js|jsx$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: `${node_modules}/babel-loader`,
+				options: {presets: config.babelPresets, plugins: config.babelPlugins}
+			},
+			...styleRules('vue'),
+			...config.rules
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		...config.webpackPlugin
+	]
 }
